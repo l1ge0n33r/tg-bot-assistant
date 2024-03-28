677f519c406d8dead8c69c43712400d66a683456
l1ge0n33r <48294455+l1ge0n33r@users.noreply.github.com>
2024-03-28 13:38:00 +0200

    Minor Update

---
+++ bot.py
@@ -0,0 +1,94 @@
+import logging
+import json
+import random
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+rfp = open('rockets.json','+r')
+tokenfp = open('token.json','r')
+token = json.load(tokenfp)
+tokenfp.close()
+rockets_count = int(json.load(rfp))
+
+
+# TODO:
+# - add todolist
+# - interface: LLM to specify commands; add questions to specify is commands correct
+# - distribute message to group of people
+# - monitoring 
+# - set name assotiations to user
+# - notification
+# - happybirthday test
+# - saving sketches (verify by user id to not junk up harddrive)
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+async def about_cmd(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Pee-pee poo-poo, imma stoopid bot")
+
+async def whoami(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=str(update.message.from_user.first_name)+" "+str(update.message.from_user.id))
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=str(update.effective_chat))
+
+async def echo(update: Update, context):
+    user_says = " ".join(context.args)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text= "You said: "+user_says)
+
+async def dice(update: Update, context):
+    if len(context.args) == 0 :
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You forgor to add number")
+    else:
+        dice = int(context.args[0])
+        result =  random.randint(1, dice)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="You rolled: "+str(result))
+
+async def todo_show(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    pass
+
+async def kill(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message.from_user.id == 737928817:
+        
+        json.dump(rockets_count, rfp)
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="Adios B-|")
+        rfp.close()
+        exit()
+    else:
+        await context.bot.send_message(chat_id=update.effective_chat.id, text="How about u?")
+
+async def rockets(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    global rockets_count
+    rockets_count= rockets_count + 1
+    json.dump(rockets_count, rfp)
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="You've sent "+str(rockets_count)+" rockets to Afrika")
+
+
+
+
+
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(token).build()
+    
+    start_handler = CommandHandler('start', start)
+    about_handler = CommandHandler('about', about_cmd)
+    whoami_handler = CommandHandler('whoami', whoami)
+    echo_handler = CommandHandler('echo', echo)
+    dice_handler= CommandHandler('dice', dice)
+    rocket_handler = CommandHandler('rocket', rockets)
+    kill_handler = CommandHandler('kys', kill)
+
+    application.add_handler(start_handler)
+    application.add_handler(about_handler)
+    application.add_handler(whoami_handler)
+    application.add_handler(echo_handler)
+    application.add_handler(dice_handler)
+    application.add_handler(rocket_handler)
+    application.add_handler(kill_handler)
+
+    application.run_polling()
---
+++ todo.py
@@ -0,0 +1,8 @@
+# Todo
+# - get list of tasks
+# - set new task
+# - remove task
+# - change task state
+# - change task
+# - set task deadline
+#
