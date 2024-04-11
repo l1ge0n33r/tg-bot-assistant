e9ee0ba9221735452675388a159613d1aec44a87
l1ge0n33r <furry.alex2003@gmail.com>
2024-04-12 00:48:57 +0300

    Revert "Sligtly updated bot.py"
    This reverts commit cb4a53c0edf316f62966cad538a62ac310d9506c.

--- todo.py
+++ todo.py
@@ -6,22 +6,3 @@
 # - change task
 # - set task deadline
 #
-
-
-def add_task(text:str):
-    pass
-
-def remove_task(id:int):
-    pass
-
-def change_task(id:int, text:str):
-    pass
-
-def show():
-    pass
-
-def get_task_list():
-    pass
-
-def get_task(id:int):
-    pass
